5f59b2e7553eabb9e7ff02f8aa6c9a30a644b3a7
MicRob <michaelrobgrove@gmail.com>
2025-09-21 13:56:45 -0400

    added rss.js

---
+++ functions/rss.js
@@ -0,0 +1,24 @@
+export async function onRequest(context) {
+  const posts = await context.env.BLOG_KV.get('posts');
+  const postsData = JSON.parse(posts || '{"posts":[]}');
+  
+  const rss = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+  <channel>
+    <title>Alfred Web Design &amp; Shirts Blog</title>
+    <description>Web design, graphic design, and custom printing insights</description>
+    <link>https://alfredwebdesign.com/blog/</link>
+    ${postsData.posts.map(post => `
+    <item>
+      <title>${post.title}</title>
+      <description>${post.excerpt}</description>
+      <link>https://alfredwebdesign.com/blog/${post.slug}</link>
+      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+    </item>`).join('')}
+  </channel>
+</rss>`;
+
+  return new Response(rss, {
+    headers: { 'Content-Type': 'application/rss+xml' }
+  });
+}
