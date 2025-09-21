2174a76cc0809c601a6a48356686b0724e19a340
MicRob <michaelrobgrove@gmail.com>
2025-09-21 19:50:16 -0400

    Update clear.js

--- functions/clear.js
+++ functions/clear.js
@@ -1,6 +1,3 @@
-export async function onRequest(context) {
-  await context.env.BLOG_KV.put('posts', '{"posts":[]}');
-  return new Response('All posts cleared!');
-}export async function onRequest() {
+export async function onRequest() {
   return Response.redirect('/admin', 302);
 }
